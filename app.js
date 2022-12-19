6f4851e62863155c0b963b350af7152b49741578
Ian Hess <ian.h.hess@gmail.com>
2022-12-19 14:57:47 -0500

    make return message to server

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
     fs.writeFileSync("message.txt", "DUMMY");
     res.statusCode = 302;
     res.setHeader("Location", "/");
