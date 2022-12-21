be3fab6991b9dc39fd1b7d3a0fafcbb5d220de0b
Ian Hess <ian.h.hess@gmail.com>
2022-12-21 15:10:49 -0500

    actually finish the push call

--- firstTest/users.js
+++ firstTest/users.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
@@ -7,7 +9,7 @@ const requestHandler = (req, res) => {
     res.write("<head><title>Howdy</title></head>");
     res.write("<body><h1>Howdy there</h1></body>");
     res.write(
-      '<body><form action="/users" method="POST"><input type="text" name="users"><button type="submit">Send</button></input></form></body>'
+      '<body><form action="/create-user" method="POST"><input type="text" name="users"><button type="submit">Send</button></input></form></body>'
     );
     res.write("</html>");
     return res.end();
@@ -19,19 +21,28 @@ const requestHandler = (req, res) => {
     res.write("</html>");
     return res.end();
   }
-  if (url === "/create-user") {
-    res.write("<html>");
-    res.write("<head><title>Howdy</title></head>");
-    res.write("<body><ul><li>Ian</li><li>John</li><li>Steve</li></ul></body>");
-    res.write("</html>");
-    return res.end();
+  if (url === "/create-user" && method === "POST") {
+    const userList = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      userList.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedList = Buffer.concat(userList).toString();
+      const message = parsedList.split("=")[1];
+      console.log(message);
+        fs.writeFile("message.txt", message, (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          return res.end();
+    });
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>Howdy</title></head>");
   res.write("<body><h1>Howdy there</h1></body>");
   res.write("</html>");
-  return res.end();
+  res.end();
 };
 
 module.exports = requestHandler;
