5d2d521fc6bb6ddf32ac89cc914e36652db32cf3
Ian Hess <ian.h.hess@gmail.com>
2022-12-22 12:04:43 -0500

    use express js for console log

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require("http");
 
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  // ...
+});
+
+const server = http.createServer(app);
 server.listen(3000);
