f51baec0502e1b62a802a66ea68f6c3b226a6437
Ian Hess <ian.h.hess@gmail.com>
2022-12-23 10:50:31 -0500

    remove test middleware

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next();
-});
 
 app.use((req, res, next) => {
   console.log("in another middleware!");
