f35a7901ffeb6444977f2826fad21e17cd7bdc94
Ian Hess <ian.h.hess@gmail.com>
2022-12-29 15:26:55 -0500

    move admin middleware

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ app.use("/", (req, res, next) => {
   next();
 });
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express</h1>");
 });
