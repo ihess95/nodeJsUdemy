cf5b03ac8c43c8bf454536afe93f3f546a34038d
Ian Hess <ian.h.hess@gmail.com>
2022-12-29 15:27:18 -0500

    add routes folder `

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { builtinModules } = require("module");
+
+const router = express.Router();
+
+router.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
+  );
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
