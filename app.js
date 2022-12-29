b7a4236a637639484d56be42c30f062f02671ee0
Ian Hess <ian.h.hess@gmail.com>
2022-12-29 13:45:31 -0500

    add form page

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res, next) => {
-  console.log("This always runs");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("in another middleware!");
-  res.send("<h1>The Add product page</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("in another middleware!");
   res.send("<h1>Hello from Express</h1>");
 });
 
